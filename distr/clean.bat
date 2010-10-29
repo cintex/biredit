04795b17c05068a8cbac68b2251168e8270d0f13
tatuich <tatuich@40d43649-0449-9339-b1e3-665f9c116910>
2010-10-29 00:08:26 +0000

    distr/clean.bat script updated

--- distr/clean.bat
+++ distr/clean.bat
@@ -1,11 +1,11 @@
+del *.dist
 del *.exe
+del *.htm
+del *.ini
+del *.upx
 del *.zip
 del changelog.txt
-del help.en.htm
-del help.ru.htm
 del license.txt
-del profile.ini
-del profile.ini.dist
 rd /S /Q lang
 rd /S /Q plugins
 rd /S /Q syn
