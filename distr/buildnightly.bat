ef7363c26e8aef83cb0c550f6505080f708ecea1
tatuich <tatuich@40d43649-0449-9339-b1e3-665f9c116910>
2010-11-14 05:46:00 +0000

    Nightly build

--- distr/buildnightly.bat
+++ distr/buildnightly.bat
@@ -3,4 +3,4 @@ del *.upx
 del *.zip
 copy /y ..\src\biredit.exe biredit.exe
 ..\utils\upx.exe biredit.exe --best -v --ultra-brute --compress-icons=0
-..\utils\7za\7za.exe a biredit-2.0.2.53-win32.zip biredit.exe
+..\utils\7za\7za.exe a biredit-2.0.2.56-win32.zip biredit.exe
